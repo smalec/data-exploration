34897de1177abd682ccf689dec4896d11f125274
Bartosz Łągwa <lagwabartosz@gmail.com>
2016-10-29 19:13:29 +0200

    [#13] Solved PS3/P0

--- blagwa/ProblemSet1/dists.m
+++ blagwa/ProblemSet1/dists.m
@@ -1,6 +1,6 @@
 function D = dists(X, Y)
-    [~, N] = size(X); % Nie mo¿na zrobiæ size(X)(2)?
-    [~, M] = size(Y);
+    N = size(X, 2); % Nie mo¿na zrobiæ size(X)(2)?
+    M = size(Y, 2);
     D = zeros(N, M);
     for i = [1 : N]
         D(i, :) = sqrt(sum((X(:, i) - Y).^2));
